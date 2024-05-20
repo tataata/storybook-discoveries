37a46fc00ca019102d43199183257689c3222bec
T <fortataata@gmail.com>
2024-05-20 21:55:29 +0200

    refactor avatar story with args

--- taskbox/src/components/Avatar.stories.jsx
+++ taskbox/src/components/Avatar.stories.jsx
@@ -3,8 +3,32 @@ import Avatar from "./Avatar";
 export default {
   title: "Avatar",
   component: Avatar,
+  // argTypes: {
+  //   variant: {
+  //     options: ["default", "rounded"],
+  //     control: { type: "radio" },
+  //   },
+  // },
 };
 
-export const Default = () => <Avatar />;
+export const Default = {
+  args: {
+    name: "Thomas Mustermann",
+    src: "https://placehold.co/200",
+    isRounded: false,
+  },
+};
 
-export const Rounded = () => <Avatar isRounded="true" />;
+export const DefaultLongName = {
+  args: {
+    ...Default.args,
+    name: "Gabriel Filipe Thomas Santo Grande",
+  },
+};
+
+export const Rounded = {
+  args: {
+    ...Default.args,
+    isRounded: true,
+  },
+};
